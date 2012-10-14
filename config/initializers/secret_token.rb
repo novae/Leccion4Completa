42b7e3b256b85c62ab31f8d982f5b3fa02684863
novaegr <novaegr@gmail.com>
2012-10-13 19:56:45 -0500

    antes de modificar Gemfile inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Residencias::Application.config.secret_token = 'd9f067f26dd729a2c69be8af30b750f5378e55c30bd64b8e33fdbc0939f3d18573b3438d3167574b88cf8fb4b91b0d3095017d599d58dbc6c811451b679b78ed'
